d475d749f63eae770c6c03f757dc33564e6c832f
mohsen <mohsen1076@gmail.com>
2022-02-05 13:47:37 +0330

    First functional test is specced out in comments, and now uses unittest module.

--- functional_tests.py
+++ functional_tests.py
@@ -1,14 +1,44 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Feb  5 10:53:34 2022
-
-@author: mohsen
-"""
-
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # Ali has heared ablout a cool new online to-do app. He goes to check 
+        # out its homepage
+        self.browser.get('http://localhost:8000')
+
+        # He notices the page title and header mention to-do lists
+        self.assertIn('To-Do', self.browser.title)
+        #self.fail('finish the test')
+        
+        # He is invited to enter a to-do item straight away
+
+        # He types "Read clean-code book" into a text box
+
+        # When he hits enter, the pages updates, and now page lists
+        # "1: Read clean-code book" as an item in a to-do list
+
+        # There is still a text box inviting him to add another item. He enters
+        # "Read TDD with python book" and hits enter
+
+        # The page updates again, and now shows both items on his list
+
+        # Ali wonders wheater the site will remember her list. Then he sees 
+        # that the site has generated a unique URL for him and there is some 
+        # explanations text for that
+
+        # He visits that URL and sees that his to-do list is still there
+
+        # Satisfies, she goes back to sleep
+
+if __name__ == '__main__':
+    unittest.main()
+    
