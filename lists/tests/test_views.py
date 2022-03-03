9de1d2e6b398ff51475a35781bbc6d8fab1c07e5
mohsen <mohsen1076@gmail.com>
2022-03-03 08:56:59 +0330

    Split out unit tests in lists app into two fiels

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -1,4 +1,3 @@
-from urllib import response
 from django.test import TestCase
 from lists.models import Item, List
 
@@ -7,36 +6,6 @@ class HomePageTest(TestCase):
     def test_view_uses_correct_html(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'lists/home.html')
-
-
-class ListAndItemModelTest(TestCase):
-    
-    def test_saving_and_retrieving_item_objects(self):
-        list_ = List()
-        list_.save()
-
-        first_item = Item()
-        first_item.text = 'This is the first item.'
-        first_item.list = list_
-        first_item.save()
-
-        second_item = Item()
-        second_item.text = 'This is the second item.'
-        second_item.list = list_
-        second_item.save()
-
-        saved_list = List.objects.first()
-        self.assertEqual(saved_list, list_)
-
-        saved_items = Item.objects.all()
-        self.assertEqual(saved_items.count(), 2)
-
-        first_saved_item = saved_items[0]
-        second_saved_item = saved_items[1]
-        self.assertEqual(first_saved_item.text, 'This is the first item.')
-        self.assertEqual(first_saved_item.list, list_)
-        self.assertEqual(second_saved_item.text, 'This is the second item.')
-        self.assertEqual(second_saved_item.list, list_)
 
 
 class ListViewTest(TestCase):
